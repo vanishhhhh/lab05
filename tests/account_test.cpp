efca5ce69499804cd89dc62b48bb7530ab113dcf
vanishhhhh <vanpanvit@gmail.com>
2026-04-16 13:20:11 +0300

    added tests for account

---
+++ tests/account_test.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include "Account.h"
+
+TEST(Account, ConstructorStoresIdAndBalance) {
+    Account account(7, 100);
+
+    EXPECT_EQ(account.id(), 7);
+    EXPECT_EQ(account.GetBalance(), 100);
+}
+
+TEST(Account, ChangeBalanceThrowsIfAccountIsNotLocked) {
+    Account account(1, 100);
+
+    EXPECT_THROW(account.ChangeBalance(50), std::runtime_error);
+    EXPECT_EQ(account.GetBalance(), 100);
+}
+
+TEST(Account, LockAllowsBalanceChange) {
+    Account account(1, 100);
+
+    account.Lock();
+    account.ChangeBalance(50);
+
+    EXPECT_EQ(account.GetBalance(), 150);
+}
+
+TEST(Account, LockAllowsNegativeBalanceChange) {
+    Account account(1, 100);
+
+    account.Lock();
+    account.ChangeBalance(-30);
+
+    EXPECT_EQ(account.GetBalance(), 70);
+}
+
+TEST(Account, DoubleLockThrowsException) {
+    Account account(1, 100);
+
+    account.Lock();
+    EXPECT_THROW(account.Lock(), std::runtime_error);
+}
+
+TEST(Account, UnlockDisablesBalanceChange) {
+    Account account(1, 100);
+
+    account.Lock();
+    account.ChangeBalance(25);
+    EXPECT_EQ(account.GetBalance(), 125);
+
+    account.Unlock();
+    EXPECT_THROW(account.ChangeBalance(10), std::runtime_error);
+    EXPECT_EQ(account.GetBalance(), 125);
+}
+
+TEST(Account, DestructorIsCalled) {
+    Account* account = new Account(5, 200);
+    EXPECT_EQ(account->GetBalance(), 200);
+    delete account;
+}
